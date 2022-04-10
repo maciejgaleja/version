a74a5a594d06e1037d7244cb5bbef29e2cf1c997
Maciej Galeja <maciej.galeja@outlook.com>
2022-04-10 17:03:51 +0200

    Fix pylint problems

--- version.py
+++ version.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
+"""Module with the program to read semver from git history."""
 
 import subprocess
 
 
 class VersionComponent:
+    """Represents single number in a version."""
 
     def __init__(self, next_component=None) -> None:
+        """Constructor."""
         self.number = 0
         self._next = next_component
 
     def set(self, number: int) -> None:
+        """Set own number and reset subsequent numbers."""
         self.number = number
         if self._next:
             self._next.set(0)
 
     def increment(self) -> None:
+        """Set own number to one higher."""
         self.set(self.number + 1)
 
     def __str__(self):
+        """Return string representation."""
         return str(self.number)
 
 
 class SemVersion:
+    """Represents semver version."""
 
     def __init__(self):
+        """Constructor."""
         self.patch = VersionComponent()
         self.minor = VersionComponent(self.patch)
         self.major = VersionComponent(self.minor)
         self.appendix: str = None
 
     def __str__(self):
+        """Retrurn string representation."""
         ret = ".".join([str(self.major), str(self.minor), str(self.patch)])
         if self.appendix:
             ret += f"-{self.appendix}"
@@ -37,11 +46,15 @@ class SemVersion:
 
 
 def read_from_git(repo_path: str = "."):
+    """Return semver constructed by reading git history."""
     ret = SemVersion()
 
     log_lines = []
-    r = subprocess.run(["git", "log"], cwd=repo_path, stdout=subprocess.PIPE)
-    log_lines = r.stdout.decode().splitlines()
+    run_res = subprocess.run(["git", "log"],
+                             cwd=repo_path,
+                             stdout=subprocess.PIPE,
+                             check=True)
+    log_lines = run_res.stdout.decode().splitlines()
     commit_lines = []
     commit_segment = None
     for line in log_lines:
@@ -59,6 +72,7 @@ def read_from_git(repo_path: str = "."):
 
 
 def _analyze_commit(commit_log, version):
+    """Look at commit information and modify version if needed."""
     is_merge = False
     version_commands = []
     for line in commit_log:
@@ -79,8 +93,9 @@ def _analyze_commit(commit_log, version):
 
 
 def main():
-    v = read_from_git()
-    print(v)
+    """CLI entry point."""
+    version = read_from_git()
+    print(version)
 
 
 if __name__ == "__main__":
